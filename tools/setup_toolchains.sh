928a8850fb1a840babd7433569b8f92ae90d1e50
Roland Dobai <roland.dobai@espressif.com>
2025-06-26 16:12:49 +0200

    change: Update the toolchain version to the latest one

--- tools/setup_toolchains.sh
+++ tools/setup_toolchains.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 TO_DOWNLOAD=(
-    "https://github.com/espressif/crosstool-NG/releases/download/esp-14.2.0_20241119/xtensa-esp-elf-14.2.0_20241119-x86_64-linux-gnu.tar.xz"
+    "https://github.com/espressif/crosstool-NG/releases/download/esp-15.1.0_20250607/xtensa-esp-elf-15.1.0_20250607-x86_64-linux-gnu.tar.xz"
     "https://dl.espressif.com/dl/xtensa-lx106-elf-gcc8_4_0-esp-2020r3-linux-amd64.tar.gz"
-    "https://github.com/espressif/crosstool-NG/releases/download/esp-14.2.0_20241119/riscv32-esp-elf-14.2.0_20241119-x86_64-linux-gnu.tar.xz"
+    "https://github.com/espressif/crosstool-NG/releases/download/esp-15.1.0_20250607/riscv32-esp-elf-15.1.0_20250607-x86_64-linux-gnu.tar.xz"
 )
 
 for url in "${TO_DOWNLOAD[@]}"; do
